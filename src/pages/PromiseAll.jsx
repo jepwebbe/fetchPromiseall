c41b2acef0e7355e77f5dff0f34f7023897ee2b9
jepwebbe <jepwebbe@gmail.com>
2022-12-16 13:31:20 +0100

    comments added to Promise.all

--- src/pages/PromiseAll.jsx
+++ src/pages/PromiseAll.jsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 export const PromiseAll = () => {
-  const [products, setProducts] = useState([]);
-  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);  // Declare state variable to store list of products
+  const [categories, setCategories] = useState([]);  // Declare state variable to store list of categories
 
   useEffect(() => {
+    // Make HTTP request to fetch list of products
     const productsEndpoint = fetch("https://dummyjson.com/products");
+    // Make HTTP request to fetch list of categories
     const categoryEndPoint = fetch("https://dummyjson.com/products/categories");
 
+    // Use Promise.all to perform requests concurrently and wait for both to resolve
     Promise.all([productsEndpoint, categoryEndPoint])
+      // the then method is called on the resulting promise, and the resolved values are passed as an argument to a function that maps over the values and returns a new array of promises that are resolved to the JSON representations of the responses
       .then((values) => Promise.all(values.map((value) => value.json())))
+      // When all JSON data is available, update state variables and re-render component
       .then((mappedValues) => {
         setProducts(mappedValues[0].products.slice(0, 10));
         setCategories(mappedValues[1].slice(0, 5));
       });
-  }, []);
+  }, []);  // Empty dependency array means this effect only runs once, on initial render
+
 
   return (
     <section>
